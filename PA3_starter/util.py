1a5b36d59ee2c4f908f3089af0f8a6494b4765fd
$uname <$adithyasamavedhi@gmail.com>
2023-02-20 20:40:52 +0000

    added correct iou

--- PA3_starter/util.py
+++ PA3_starter/util.py
@@ -2,29 +2,25 @@ import numpy as np
 import torch
 
 def iou(pred, target, n_classes = 21):
-    # pred = pred.view(-1)
+    ious = []
     pred= torch.argmax(pred, dim =1)
-    pred = pred.reshape(pred.shape[0], -1)
-    target = target.reshape(target.shape[0], -1)
-    # taget = pred.view(-1)
-    iou_scores = []
-    for ind in range(pred.shape[0]):
-        iou_class = []
-        for c in range(n_classes):
-            intersection = ((pred[ind]==c)*(target[ind]==c)).sum()
-            union = ((pred[ind]==c)+(target[ind]==c)).sum()
-            if union ==0:
-                # iou_class = torch.cat((iou_class, 0))
-                iou_class.append(0)
-            else:
-                # print(intersection/union)
-                # iou_class = torch.cat((iou_class, (intersection/union)), axis=0)
-                iou_class.append(intersection/union)
-        # iou_scores = torch.cat((iou_scores, torch.mean(iou_class)), axis=0)
+    pred = pred.view(-1)
+    target = target.view(-1)
 
-        iou_scores.append(torch.mean(torch.Tensor(iou_class)))
+    # Ignore IoU for undefined class ("9")
+    for cls in range(n_classes-1):  # last class is ignored
 
-    return torch.mean(torch.Tensor(iou_scores))
+        pred_inds = pred == cls
+        target_inds = target == cls
+        intersection = torch.sum(pred_inds & target_inds)
+        union = torch.sum(pred_inds | target_inds)
+        if float(union) == 0:
+            ious.append(float('nan'))  # If there is no ground truth, do not include in evaluation
+        else:
+            ious.append(float(intersection / union))
+
+
+    return torch.nanmean(torch.Tensor(ious))
 
     # for c in range(n_classes):
     #     intersection = ((pred==c)*(target==c)).sum(dim = 1)
